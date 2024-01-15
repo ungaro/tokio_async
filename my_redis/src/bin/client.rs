ba327944009e9b256cf0572aa6a673e90d8ffc18
Alp Guneysel <alp.guneysel@gmail.com>
2024-01-14 23:06:28 -0500

    mspc

--- my_redis/src/bin/client.rs
+++ my_redis/src/bin/client.rs
@@ -1,7 +1,58 @@
 use mini_redis::client;
+use bytes::Bytes;
+
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+    }
+}
+
 
 #[tokio::main]
 async fn main() {
+
+
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+
+    tokio::spawn(async move {
+        tx.send("sending from first handle").await;
+    });
+
+    tokio::spawn(async move {
+        tx2.send("sending from second handle").await;
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {}", message);
+    }
+
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+    
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
+    });
+
     // Establish a connection to the server
     let mut client = client::connect("127.0.0.1:6379").await.unwrap();
 
